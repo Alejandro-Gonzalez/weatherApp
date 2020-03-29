ecd664e00abcc8f08b783ecdff648185ac4255d3
Pablo Guzmán <guzman.pablo87@gmail.com>
2020-03-29 16:58:07 -0300

    feat(Header): link to first option added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useContext } from 'react';
+import { Title, Link } from './styles';
+import { Context } from 'context';
 
-const Header = () => <Title>WeatherApp</Title>;
+const Header = () => {
+	const {
+		cities: { codes }
+	} = useContext(Context);
+
+	return (
+		<Link to={`/${codes[0]}`}>
+			<Title>WeatherApp</Title>
+		</Link>
+	);
+};
 
 export default Header;
