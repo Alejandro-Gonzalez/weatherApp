d05b68c650dbf5a56cc057b170885338da93962a
Pablo Guzmán <guzman.pablo87@gmail.com>
2020-03-29 16:56:35 -0300

    feat(ErrorMessage): message prop added

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Container, Message, Image } from './styles';
+import { string } from 'prop-types';
 
-const ErrorMessage = ({ ...rest }) => (
+const ErrorMessage = ({ message, ...rest }) => (
 	<Container {...rest}>
 		<Image />
-		<Message>Algo salío mal</Message>
+		<Message>{message}</Message>
 	</Container>
 );
+
+ErrorMessage.propTypes = {
+	message: string
+};
+
+ErrorMessage.defaultProps = {
+	message: 'Algo salío mal'
+};
 
 export default ErrorMessage;
