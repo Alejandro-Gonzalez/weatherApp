403e5d4b68cf84c19008b19b42877f0fc9fb70dc
Pablo Guzmán <guzman.pablo87@gmail.com>
2020-03-29 13:29:25 -0300

    feat(Context): version added

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,5 @@
+import { version } from '../../package.json';
+
+export const APP_VERSION = version;
 export const API_URL = process.env.WEATHER_API;
 export const API_KEY = process.env.WEATHER_KEY;
--- src/context/index.js
+++ src/context/index.js
@@ -1,12 +1,14 @@
 import React, { createContext } from 'react';
 import useCities from './cities';
 import useWeather from './weather';
+import { APP_VERSION } from 'config';
 
 export const Context = createContext(null);
 
 export const Provider = ({ children }) => {
 	const cities = useCities();
 	const weather = useWeather(cities);
+	const version = APP_VERSION;
 
-	return <Context.Provider value={{ cities, weather }}>{children}</Context.Provider>;
+	return <Context.Provider value={{ version, cities, weather }}>{children}</Context.Provider>;
 };
