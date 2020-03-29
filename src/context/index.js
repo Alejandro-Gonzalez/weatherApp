be51d15468b1d6b29cc849a9bac94520f2f4e253
Pablo Guzmán <guzman.pablo87@gmail.com>
2020-03-29 17:49:43 -0300

    feat(Context): key proyect added

--- src/context/index.js
+++ src/context/index.js
@@ -1,14 +1,17 @@
 import React, { createContext } from 'react';
 import useCities from './cities';
 import useWeather from './weather';
-import { APP_VERSION } from 'config';
+import { APP_VERSION, APP_NAME } from 'config';
 
 export const Context = createContext(null);
 
 export const Provider = ({ children }) => {
 	const cities = useCities();
 	const weather = useWeather(cities);
-	const version = APP_VERSION;
+	const proyect = {
+		version: APP_VERSION,
+		name: APP_NAME
+	};
 
-	return <Context.Provider value={{ version, cities, weather }}>{children}</Context.Provider>;
+	return <Context.Provider value={{ proyect, cities, weather }}>{children}</Context.Provider>;
 };
